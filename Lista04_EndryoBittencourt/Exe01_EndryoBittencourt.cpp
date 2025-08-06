2580d4cb92d53e076c1c8433de57e4f5cb6390a0
EndryoBittencourt <endryo.jogos@outlook.com>
2025-08-06 13:41:06 -0300

    Update Exe01_EndryoBittencourt.cpp

--- Lista04_EndryoBittencourt/Exe01_EndryoBittencourt.cpp
+++ Lista04_EndryoBittencourt/Exe01_EndryoBittencourt.cpp
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 int main() {
-printf("\nEndryo Gabriel Bittencourt\n");
-float salario = 1000.0; // salaria do começo
-float percentual = 1.5; // aumento
+float salario = 1000.0; // Salário inicial em 2018
+float percentual = 1.5; // Aumento inicial em 2019
 int ano_atual;
 
 printf("Digite o ano atual: ");
 scanf("%d", &ano_atual);
 
-if (ano_atual <2018){
-    printf("Ano deve ser 2018 ou posterior.\n");
-    return 1;
+if (ano_atual < 2018) {
+printf("Ano deve ser 2018 ou posterior.\n");
+return 1;
 }
-//faz o aumento de 2018 ate o ano atual.
-for (int ano = 2018; ano <= ano_atual; ano++){
-    salario = salario + (salario * (percentual / 100.0));
-    
-//apartir de 2019 o percentual dobra.
-if (ano >= 2019){
-    percentual = percentual * 2;
-}
+
+// Aplica os aumentos a partir de 2019
+for (int ano = 2019; ano <= ano_atual; ano++) {
+salario = salario + (salario * (percentual / 100.0));
+percentual = percentual * 2; // dobra o percentual a cada ano após 2019
 }
 
 printf("Salario em %d: R$ %.2f\n", ano_atual, salario);
